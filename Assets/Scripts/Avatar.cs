b7112a25eef024ef63d23e83618912c0a7c6ef4e
Adonis Gomez <x1852628r@iespoblenou.org>
2017-06-06 22:52:09 +0200

    Last commit

---
+++ Assets/Scripts/Avatar.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Avatar : MonoBehaviour {
+
+	public ParticleSystem shape, trail, burst;
+    public GameObject ship;
+	private Player player;
+
+	public float deathCountdown = -1f;
+
+	private void Awake () {
+		player = transform.root.GetComponent<Player>();
+        ship = GameObject.Find("StarShip");
+        MeshRenderer render = ship.GetComponent<MeshRenderer>();
+        render.enabled = true;
+    }
+
+	private void OnTriggerEnter (Collider collider) {
+		if (deathCountdown < 0f) {
+			shape.enableEmission = false;
+			trail.enableEmission = false;
+            ship = GameObject.Find("StarShip");
+            MeshRenderer render = ship.GetComponent<MeshRenderer>();
+            render.enabled = false;
+            burst.Emit(burst.maxParticles);
+			deathCountdown = burst.startLifetime;
+		}
+	}
+	
+	private void Update () {
+		if (deathCountdown >= 0f) {
+			deathCountdown -= Time.deltaTime;
+			if (deathCountdown <= 0f) {
+				deathCountdown = -1f;
+				shape.enableEmission = true;
+				trail.enableEmission = true;
+				player.Die();
+			}
+		}
+	}
+}
